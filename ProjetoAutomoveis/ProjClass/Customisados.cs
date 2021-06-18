77d031ad7c1eb8a3212a7af2b332fb01a0d60fee
mbreslawskij <85262653+mbreslawskij@users.noreply.github.com>
2021-06-17 23:17:54 -0300

    Add files via upload

---
+++ ProjetoAutomoveis/ProjClass/Customisados.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjClass
+{
+    public class Customisados
+    {
+        public int AnoModelo = 1994;
+        public int Cilindradas = 1900;
+        public string Montadora = "VW";
+        public string Alteracoes = "Turbo interculado";
+        public string Cor = "Vermelho";
+
+    }
+}
---
+++ ProjetoAutomoveis/ProjClass/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ProjClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string motorista = "Airton Sena";
+
+            Customisados golT = new Customisados();
+            golT.AnoModelo = 2012;
+            golT.Cilindradas = 1000;
+            golT.Alteracoes = "Original";
+
+            Esportivos ferrari = new Esportivos();
+            ferrari.AnoModelo = 2018;
+            ferrari.Cilindradas = 6496;
+            ferrari.Cor = "Vermelha";
+            ferrari.Alteracoes = "Rodas";
+
+            CLassicos dodge = new CLassicos();
+            dodge.AnoModelo = 1978;
+            dodge.Cilindradas = 5212;
+            dodge.Cor = "Preto";
+
+        }
+    }
+}
